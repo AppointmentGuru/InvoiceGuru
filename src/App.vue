23f5f777dfa0e3eff60b52b3edfbf083b0e2c880
toast38coza <info@38.co.za>
2017-03-01 13:38:41 +0200

    Invoicebuilder component setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <invoice-builder v-model='invoice' ></invoice-builder>
+    <strong>Result:</strong>
+    <pre>{{invoice}}</pre>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
+import InvoiceBuilder from './components/InvoiceBuilder'
 export default {
   name: 'app',
-  components: {
-    Hello
+  components: { InvoiceBuilder },
+  data () {
+    return {
+      invoice: {}
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/InvoiceBuilder.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="invoice-builder">
+    <textarea v-model='invoice' >
+    </textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InvoiceBuilder',
+  props: {
+    value: { type: Object, required: true }
+  },
+  data () {
+    return {
+      invoice: JSON.stringify(this.value)
+    }
+  },
+  watch: {
+    invoice () {
+      let data = null
+      try {
+        data = JSON.parse(this.invoice)
+      } catch (err) {
+        console.log(err)
+        data = 'Not valid JSON'
+      }
+      this.$emit('input', data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
