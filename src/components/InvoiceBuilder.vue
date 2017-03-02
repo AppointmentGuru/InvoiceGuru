767c7dcaf20d110215b42562f48d7de65d965da6
Kangwa Bwali <kangwa2003@gmail.com>
2017-03-02 16:21:06 +0200

    - added element vue components - added table to invoice builder component

--- src/components/InvoiceBuilder.vue
+++ src/components/InvoiceBuilder.vue
@@ -1,9 +1,60 @@
 <template>
   <div class="invoice-builder">
-    <textarea v-model='invoice' style='width:400px;height:400px;' >
-    </textarea>
-    <div v-show='error' >{{error}}</div>
-  </div>
+    <el-row type="flex" :gutter="20" justify="space-between">
+      <el-col :span="8">
+        <img :src="value.data.logo" />
+      </el-col>
+      <el-col :span="4">
+        <h2>INVOICE</h2> <br/>
+      </el-col>
+    </el-row>
+
+    <el-row type="flex" :gutter="20" justify="space-between">
+      <el-col :span="6">
+        Bill To:
+        <pre>{{value.data.to}}</pre>
+      </el-col>
+      <el-col :span="6">
+        <table>
+          <tbody>
+            <tr>
+              <td>Date:</td>
+              <td>{{value.data.date}}</td>
+            </tr>
+            <tr>
+              <td>Due Date:</td>
+              <td>{{value.data.due_date}}</td>
+            </tr>
+            <tr>
+              <td>Balance Due:</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="24">
+        <div class="grid-content bg-purple">
+          <el-table :data="value.data.items" stripe style="width: 100%">
+            <el-table-column prop="name" label="Item"></el-table-column>
+            <el-table-column prop="description" label="Description" width="300px"></el-table-column>
+            <el-table-column prop="quantity" label="Quantity"></el-table-column>
+            <el-table-column prop="unit_cost" label="Rate"></el-table-column>
+            <el-table-column label="Amount">
+            <template scope="scope">
+              <span style="margin-left: 10px">{{ amount(scope.row.unit_cost, scope.row.quantity) }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </el-col>
+  </el-row>
+
+  <textarea v-model='invoice' style='width:400px;height:400px;'></textarea>
+  <div v-show='error' >{{error}}</div>
+</div>
 </template>
 
 <script>
@@ -16,6 +67,11 @@ export default {
     return {
       invoice: JSON.stringify(this.value),
       error: false
+    }
+  },
+  methods: {
+    amount (cost, qty) {
+      return parseFloat(cost) * parseFloat(qty)
     }
   },
   watch: {
@@ -56,4 +112,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { Row, Col, Table, TableColumn } from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
 
 Vue.config.productionTip = false
 
