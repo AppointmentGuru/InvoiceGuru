a729b97c7451c4f2d9d328d00e21b241552dc8d0
Kangwa Bwali <kangwa2003@gmail.com>
2017-03-03 22:07:03 +0200

    added crud functionality

--- src/components/InvoiceBuilder.vue
+++ src/components/InvoiceBuilder.vue
@@ -8,7 +8,7 @@
         <h2>INVOICE</h2> <br/>
       </el-col>
     </el-row>
-
+    <br/><br>
     <el-row type="flex" :gutter="20" justify="space-between">
       <el-col :span="6">
         Bill To:
@@ -36,25 +36,49 @@
 
     <el-row :gutter="20">
       <el-col :span="24">
-        <div class="grid-content bg-purple">
-          <el-table :data="value.data.items" stripe style="width: 100%">
-            <el-table-column prop="name" label="Item"></el-table-column>
-            <el-table-column prop="description" label="Description" width="300px"></el-table-column>
-            <el-table-column prop="quantity" label="Quantity"></el-table-column>
-            <el-table-column prop="unit_cost" label="Rate"></el-table-column>
-            <el-table-column label="Amount">
+        <el-table :data="value.data.items" style="width: 100%">
+          <el-table-column prop="name" label="Item" width="300px">
+            <template scope="scope">
+              <el-input placeholder="Please input" v-model="scope.row.name"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column prop="description" label="Description" width="300px">
+            <template scope="scope">
+              <el-input placeholder="Please input" v-model="scope.row.description"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column prop="quantity" label="Quantity">
+            <template scope="scope">
+              <el-input placeholder="Please input" v-model="scope.row.quantity"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column prop="unit_cost" label="Rate">
+            <template scope="scope">
+              <el-input placeholder="Please input" v-model="scope.row.unit_cost"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column label="Amount">
             <template scope="scope">
               <span style="margin-left: 10px">{{ amount(scope.row.unit_cost, scope.row.quantity) }}</span>
             </template>
           </el-table-column>
-        </el-table>
-      </div>
-    </el-col>
-  </el-row>
+          <el-table-column label="">
+            <template scope="scope">
+              <el-button
+              size="small"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+            </template>
+          </el-table-column>
 
-  <textarea v-model='invoice' style='width:400px;height:400px;'></textarea>
-  <div v-show='error' >{{error}}</div>
-</div>
+        </el-table>
+        <el-button type="primary" @click="handleCreate">Add line item</el-button>
+      </el-col>
+    </el-row>
+    <br/><br/>
+    <textarea v-model='invoice' style='width:400px;height:400px;'></textarea>
+    <div v-show='error' >{{error}}</div>
+  </div>
 </template>
 
 <script>
@@ -72,6 +96,20 @@ export default {
   methods: {
     amount (cost, qty) {
       return parseFloat(cost) * parseFloat(qty)
+    },
+    handleCreate (index, row) {
+      this.value.data.items.push({
+        name: null,
+        quantity: 1,
+        unit_cost: null,
+        description: null
+      })
+    },
+    handleEdit (index, row) {
+      console.log(index, row)
+    },
+    handleDelete (index, row) {
+      this.value.data.items.splice(index, 1)
     }
   },
   watch: {
@@ -94,7 +132,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 h1, h2 {
   font-weight: normal;
 }
@@ -112,5 +150,28 @@ li {
 a {
   color: #42b983;
 }
+
+th.is-leaf:first-child {
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+
+th.is-leaf:last-child {
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+
+th.is-leaf, th .cell {
+  background-color: #000000 !important;
+  color: #ffffff !important;
+}
+
+.el-table, .el-table::after, .el-table::before {
+  border: none !important;
+  margin-top: 60px;
+  background-color: #fff !important;
+}
+
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import { Row, Col, Table, TableColumn } from 'element-ui'
+import { Row, Col, Table, TableColumn, Button, Input } from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
 Vue.component(Row.name, Row)
 Vue.component(Col.name, Col)
 Vue.component(Table.name, Table)
 Vue.component(TableColumn.name, TableColumn)
+Vue.component(Button.name, Button)
+Vue.component(Input.name, Input)
 
 Vue.config.productionTip = false
 
