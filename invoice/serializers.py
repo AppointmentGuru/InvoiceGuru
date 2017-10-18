16c75158d7ebe300f422f4be2567fbcbf7c66857
toast38coza <info@38.co.za>
2017-10-18 17:23:50 +0200

    add from/to to invoice payload

--- invoice/serializers.py
+++ invoice/serializers.py
@@ -4,4 +4,4 @@ from .models import Invoice
 class InvoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invoice
-        fields = ['id', 'practitioner_id', 'customer_id', 'title', 'status', 'context', 'template', 'password']
+        fields = ['id', 'practitioner_id', 'customer_id', 'title', 'status', 'context', 'template', 'password', 'invoice_period_from', 'invoice_period_to']
